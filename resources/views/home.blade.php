a43003ed621f061947a74d43f88416aed129b04a
karim <karimcastillogarnica@gmail.com>
2023-09-18 17:19:01 -0500

    Archivos base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="puntos">
+        <input type="radio">
+        <input type="radio">
+    </div>
+    <div class="container_title">
+        <p class="title_bold">Tu mejor amigo</p>
+        <h1 class="title_normal">en casa!</h1>
+    </div>
+    <div class="texto_centro">Adoptar la mascota perfecta de más de 700 refugios de animales ...</div>
+    <div class="imagen"><img src="{{asset ('imagenes/slide1.png')}}" alt="#"></div>
+    <div class="flecha"> <img src="{{asset('imagenes/chevron-right.svg')}}" alt="#"></div>
+</div>
+@endsection
